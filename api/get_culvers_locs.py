3dfcbcb050bef042ce59912ec73744151943c151
wsepesi <sepesi.w@wustl.edu>
2024-09-23 15:22:14 -0500

    subheadings on multiple / city

--- api/get_culvers_locs.py
+++ api/get_culvers_locs.py
@@ -1,9 +1,13 @@
 import requests
 
 from rich.table import Table
+from rich import box
+from itertools import groupby
 from geopy.distance import distance
 
+
 def get_json_from_lat_long(lat, long, radius=600000, limit=10):
+    # lat, long = (43.06105, -89.50676)
     url = 'https://www.culvers.com/api/locator/getLocations'
     params = {
         'lat': lat,
@@ -91,24 +95,56 @@ def get_table_data(data, location="Eden Prairie"):
     return table_data
 
 def create_table(data, location="Eden Prairie", coords=(45.676998, -111.042931)):
-    # Create a table
-    table = Table(title="Culvers Flavors of the Day Close to " + location)
-
-    # Add columns (excluding 'hrs' and 'driveThruHrs')
-    columns = ['location', 'flavor of the day', 'coordinates', 'distance']
-    for column in columns:
-        table.add_column(column.capitalize(), no_wrap=True)
-
-    # Add rows
+    # Calculate distances and sort data
     for item in data:
-        table.add_row(
-            item['city'] + ', ' + item['state'],
-            _handle_flavor(item['flavor']),
-            _handle_coords(item['coordinates']),
-            _handle_dist(distance(coords, _flip_coords(item['coordinates'])).miles)
-        )
-
-    return table
+        item['distance'] = distance(coords, _flip_coords(item['coordinates'])).miles
+    
+    sorted_data = sorted(data, key=lambda x: (x['city'], x['state'], x['distance']))
+    
+    # Group the data by city and state
+    grouped_data = groupby(sorted_data, key=lambda x: (x['city'], x['state']))
+    
+    # Create the main table
+    main_table = Table(title=f"Culvers Flavors of the Day Close to {location}", box=box.SQUARE)
+    main_table.add_column("Location", no_wrap=True)
+    main_table.add_column("Flavor of the day")
+    main_table.add_column("Coordinates")
+    main_table.add_column("Distance", justify="right")
+    
+    # Sort cities by their closest location
+    city_distances = []
+    for (city, state), group in grouped_data:
+        group_list = list(group)
+        min_distance = min(item['distance'] for item in group_list)
+        city_distances.append(((city, state), group_list, min_distance))
+    
+    city_distances.sort(key=lambda x: x[2])
+    
+    for (city, state), group_list, _ in city_distances:
+        if len(group_list) == 1:
+            # Single location in the city
+            item = group_list[0]
+            if "Coming Soon!" not in item['description']:
+                main_table.add_row(
+                    f"[bold]{city}, {state}[/bold]",
+                    _handle_flavor(item['flavor']),
+                    _handle_coords(item['coordinates']),
+                    _handle_dist(item['distance'])
+                )
+        else:
+            # Multiple locations in the city
+            main_table.add_row(f"[bold]{city}, {state}[/bold]", "", "", "")
+            for item in group_list:
+                if "Coming Soon!" not in item['description']:
+                    road = item['description'].split(' - ')[-1]
+                    main_table.add_row(
+                        f"  {road}",
+                        _handle_flavor(item['flavor']),
+                        _handle_coords(item['coordinates']),
+                        _handle_dist(item['distance'])
+                    )
+    
+    return main_table
 
 def get_table_from_zip(zip='55347'):
     json_data = get_json_from_zip(zip)
